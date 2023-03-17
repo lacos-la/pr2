c2c1be79514a0b3cdf30d1352664ee426bee2a83
lacos <lacoslaiden1@yandex.ru>
2023-03-17 19:36:31 +0300

    обновлен main.py

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ img = qrcode.make('GitHub Webhook')
 img.save("gitHub_webhook.png")
 #this string create before installation ngrok
 #эта строка для 3-й попытки подключить github к jenkins
+#гитхаб снова ругается, пробую это исправить....
