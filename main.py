167845bc3d8aa0c3454cdd047a8f64896cf572ab
lacos <lacoslaiden1@yandex.ru>
2023-03-17 19:38:30 +0300

    обновлен main.py

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ img.save("gitHub_webhook.png")
 #this string create before installation ngrok
 #эта строка для 3-й попытки подключить github к jenkins
 #гитхаб снова ругается, пробую это исправить....
+#ммммммммммммм моя любимая 403 ошибка, вот в чем дело было, класс. Включил впн.
