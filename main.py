cafb842bd97cf5ae56cd4d07be9f0c3a22cb2576
lacos <lacoslaiden1@yandex.ru>
2023-03-17 19:33:51 +0300

    обновлен main.py

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import qrcode
 img = qrcode.make('GitHub Webhook')
 img.save("gitHub_webhook.png")
 #this string create before installation ngrok
+#эта строка для 3-й попытки подключить github к jenkins
